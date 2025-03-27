cd5659205d478b3a2579974284c66055cc4b0d22
camoverride <camoverride@gmail.com>
2025-03-26 18:50:54 -0700

    picam settings

--- _image_processing_utils.py
+++ _image_processing_utils.py
@@ -30,7 +30,12 @@ if config["camera_type"] == "picam":
 
     # Initialize the picamera
     picam2 = Picamera2()
-    picam2.configure(picam2.create_preview_configuration(main={"format": "RGB888",}))
+    picam2.configure(picam2.create_preview_configuration(
+                     main={"size": (1920, 1080)},
+                     lores={"size": (640, 480)},  # Required for some reason
+                     display="main"  # Direct display output
+                    )
+                    )
                                                                 # "size": (WIDTH, HEIGHT)}))
     picam2.start()
 
