0849cc0f0a5b45b99a58f1b9d28063ec225f7906
camoverride <camoverride@gmail.com>
2025-03-26 18:38:43 -0700

    relax head direction params

--- _image_processing_utils.py
+++ _image_processing_utils.py
@@ -379,16 +379,16 @@ def is_face_looking_forward(face_landmarks: List[int],
     z = angles[2] * 360
 
     # Check which way the face is oriented.
-    if y < -3: # Looking Left
+    if y < -4: # Looking Left
         looking_forward = False
 
-    elif y > 3: # Looking Right
+    elif y > 4: # Looking Right
         looking_forward = False
 
-    elif x < -3: # Looking Down
+    elif x < -4: # Looking Down
         looking_forward = False
 
-    elif x > 7: # Looking Up
+    elif x > 8: # Looking Up
         looking_forward = False
 
     else: # Looking Forward
