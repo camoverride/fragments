55a7cb9e3849a755d83b130c4c2ecf7a0a4b6282
camoverride <camoverride@gmail.com>
2025-03-30 13:48:42 -0700

    alt fullscreen style

--- run_display.py
+++ run_display.py
@@ -459,8 +459,8 @@ if __name__ == "__main__":
     os.system("unclutter -idle 0 &")
 
     # Set screen properties
-    # cv2.namedWindow("Animation", cv2.WINDOW_NORMAL)
-    cv2.setWindowProperty("Animation", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+    cv2.namedWindow("Animation", cv2.WINDOW_NORMAL)
+    # cv2.setWindowProperty("Animation", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
     # 1. Create a named window FIRST (critical step!)
 
     cv2.imshow("Animation", cv2.imread("mona_lisa_1080_1920.jpg"))
